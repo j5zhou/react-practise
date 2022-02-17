17a6af18403d35409bddf596ba8280db717d712b
Patrick-FRE <patrick.lin@antra.com>
2022-02-17 15:22:14 -0500

    update

--- eventapp/src/components/EventApp/EventApp.js
+++ eventapp/src/components/EventApp/EventApp.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { getAllEvents } from '../../services/event.api';
 import {
-  convertTimestampToDateValue,
-  convertDateValueToTimeStamp,
-} from '../../utils/date.helper';
+  getAllEvents,
+  addNewEvent,
+  deleteEvent,
+} from '../../services/event.api';
+
+import { EventData } from '../../models/EventData';
+
 class EventApp extends React.Component {
   state = {
     events: [],
     dataCol: ['Event Name', 'Start Date', 'End Date', 'Actions'],
     isShowAddEventRow: false,
-    newEvent: {
-      eventName: '',
-      startDate: '',
-      endDate: '',
-    },
+    newEvent: new EventData('', '' + Date.now(), '' + Date.now()),
   };
-  componentDidMount() {
+
+  fetchAllEvents = () => {
     getAllEvents().then((data) => {
+      const events = data.map(({ eventName, startDate, endDate, id }) => {
+        const newEvent = new EventData(eventName, startDate, endDate, id);
+        newEvent.isEditing = false;
+        newEvent.editEvent = new EventData(eventName, startDate, endDate, id);
+        return newEvent;
+      });
+
       this.setState({
-        events: data,
+        events,
       });
     });
+  };
+
+  componentDidMount() {
+    this.fetchAllEvents();
   }
 
   hanldeAddEvent = () => {
@@ -28,12 +39,79 @@ class EventApp extends React.Component {
       isShowAddEventRow: true,
     });
   };
-  hanldeOnChange = ({ target: { name, value, type } }) => {
+  hanldeOnChange = ({ target: { name, value } }) => {
     this.setState({
       newEvent: {
         ...this.state.newEvent,
-        [name]: type === 'date' ? convertDateValueToTimeStamp(value) : value,
+        [name]: value,
       },
+    });
+  };
+
+  hanldeDelete = (id) => {
+    deleteEvent(id)
+      .then(() => {
+        this.fetchAllEvents();
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  };
+
+  handleClose = () => {
+    this.setState({
+      isShowAddEventRow: false,
+      newEvent: new EventData('', '' + Date.now(), '' + Date.now()),
+    });
+  };
+
+  hanldeSaveAddNew = () => {
+    const { eventName, startDate, endDate } = this.state.newEvent;
+    const newEvent = new EventData(eventName, startDate, endDate);
+    newEvent.parseTimeStamp();
+    if (newEvent.isValidForSave()) {
+      addNewEvent(newEvent).then((data) => {
+        //this.setState({ events: [data, ...this.state.events] });
+        this.fetchAllEvents();
+      });
+      this.handleClose();
+    } else {
+      alert('inValid');
+    }
+  };
+  hanldeEdit = (id) => {
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, isEditing: true };
+        } else {
+          return event;
+        }
+      }),
+    });
+  };
+
+  hanldeOnChangeEdit = ({ target: { name, value } }, id) => {
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, editEvent: { ...event.editEvent, [name]: value } };
+        } else {
+          return event;
+        }
+      }),
+    });
+  };
+
+  hanldeCancel = (id) => {
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, isEditing: false };
+        } else {
+          return event;
+        }
+      }),
     });
   };
 
@@ -52,31 +130,60 @@ class EventApp extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.events?.map((event) => (
-              <tr key={event.id}>
-                <td>
-                  <input type="text" disabled value={event.eventName} />
-                </td>
-                <td>
-                  <input
-                    type="date"
-                    disabled
-                    value={convertTimestampToDateValue(event.startDate)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="date"
-                    disabled
-                    value={convertTimestampToDateValue(event.endDate)}
-                  />
-                </td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            ))}
+            {this.state.events?.map((event) =>
+              event.isEditing ? (
+                <tr key={event.id}>
+                  <td>
+                    <input
+                      type="text"
+                      name="eventName"
+                      value={event.editEvent.eventName}
+                      onChange={(e) => this.hanldeOnChangeEdit(e, event.id)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="date"
+                      name="startDate"
+                      value={event.editEvent.startDate}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="date"
+                      name="endDate"
+                      value={event.editEvent.endDate}
+                    />
+                  </td>
+                  <td>
+                    <button>Save</button>
+                    <button onClick={() => this.hanldeCancel(event.id)}>
+                      Cancel
+                    </button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={event.id}>
+                  <td>
+                    <input type="text" disabled value={event.eventName} />
+                  </td>
+                  <td>
+                    <input type="date" disabled value={event.startDate} />
+                  </td>
+                  <td>
+                    <input type="date" disabled value={event.endDate} />
+                  </td>
+                  <td>
+                    <button onClick={() => this.hanldeEdit(event.id)}>
+                      Edit
+                    </button>
+                    <button onClick={() => this.hanldeDelete(event.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
           <tfoot>
             {this.state.isShowAddEventRow ? (
@@ -93,9 +200,7 @@ class EventApp extends React.Component {
                   <input
                     onChange={this.hanldeOnChange}
                     type="date"
-                    value={convertTimestampToDateValue(
-                      this.state.newEvent.startDate
-                    )}
+                    value={this.state.newEvent.startDate}
                     name="startDate"
                   />
                 </td>
@@ -104,26 +209,12 @@ class EventApp extends React.Component {
                     onChange={this.hanldeOnChange}
                     name="endDate"
                     type="date"
-                    value={convertTimestampToDateValue(
-                      this.state.newEvent.endDate
-                    )}
+                    value={this.state.newEvent.endDate}
                   />
                 </td>
                 <td>
-                  <button
-                    onClick={() => {
-                      console.log('click');
-                      this.setState({
-                        newEvent: {
-                          ...this.state.newEvent,
-                          eventName: 'Hello',
-                        },
-                      });
-                    }}
-                  >
-                    Save
-                  </button>
-                  <button>Close</button>
+                  <button onClick={this.hanldeSaveAddNew}>Save</button>
+                  <button onClick={this.handleClose}>Close</button>
                 </td>
               </tr>
             ) : null}
--- eventapp/src/services/event.api.js
+++ eventapp/src/services/event.api.js
@@ -3,3 +3,24 @@ const path = 'events';
 
 export const getAllEvents = () =>
   fetch([baseURL, path].join('/')).then((response) => response.json());
+
+export const addNewEvent = (newEvent) =>
+  fetch([baseURL, path].join('/'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(newEvent),
+  }).then((response) => response.json());
+
+export const deleteEvent = (id) =>
+  fetch([baseURL, path, id].join('/'), {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
